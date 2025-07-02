197df783345a9799240ffa94ab0bb94956254cf7
kdix-23-240 <akihide.3kami@gmail.com>
2025-07-03 01:34:23 +0900

    GameSceneMVPの記述(まだトリガーを実装していないので動かない)

--- Assets/Script/SceneManager/GameScenePresenter.cs
+++ Assets/Script/SceneManager/GameScenePresenter.cs
@@ -5,7 +5,8 @@ using UnityEngine.SceneManagement;
 public class GameScenePresenter : MonoBehaviour
 {
     private GameSceneModel model;
-    [SerializeField] private Scene sceneManager; // シーン管理用のコンポーネントを参照
+    [SerializeField] private GameClearSceneView gameClearSceneView;
+    [SerializeField] private GameOverSceneView gameOverSceneView;
 
     void Awake()
     {
@@ -14,12 +15,37 @@ public class GameScenePresenter : MonoBehaviour
 
    void Start()
     {
+        gameClearSceneView.Initialize();
+        gameOverSceneView.Initialize();
 
         Bind();
     }
 
     private void Bind()
     {
-
+        model.GameSceneStateProp
+            .Subscribe(state =>
+            {
+                switch (state)
+                {
+                    case GameSceneModel.GameSceneState.GameStart:
+                        gameClearSceneView.Hide();
+                        gameOverSceneView.Hide();
+                        break;
+                    case GameSceneModel.GameSceneState.GamePlaying:
+                        gameClearSceneView.Hide();
+                        gameOverSceneView.Hide();
+                        break;
+                    case GameSceneModel.GameSceneState.GameOver:
+                        gameClearSceneView.Hide();
+                        gameOverSceneView.Show();
+                        break;
+                    case GameSceneModel.GameSceneState.GameClear:
+                        gameOverSceneView.Hide();
+                        gameClearSceneView.Show();
+                        break;
+                }
+            })
+            .AddTo(this);
     }
 }
