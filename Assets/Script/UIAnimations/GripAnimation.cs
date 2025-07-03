b711eecd1cd60f4175fc4687e88a583dca871e14
kdix-23-240 <akihide.3kami@gmail.com>
2025-07-04 03:11:19 +0900

    握りアニメーションのいインポートと配置

---
+++ Assets/Script/UIAnimations/GripAnimation.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class GripAnimation : MonoBehaviour
+{
+    void Start()
+    {
+        Hide(); // ‰Šúó‘Ô‚Å‚Í”ñ•\¦
+    }
+
+    private void Show()
+    {
+
+    }
+
+    private void Hide()
+    {
+
+    }
+}
