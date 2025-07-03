3176abce4494e136254335995b24102ad0b7daa1
kdix-23-240 <akihide.3kami@gmail.com>
2025-07-04 01:53:00 +0900

    複数のオブジェクトやUIの色や座標の変更

--- Assets/Script/SelectBase/SelectBaseView.cs
+++ Assets/Script/SelectBase/SelectBaseView.cs
@@ -5,27 +5,8 @@ using UnityEngine;
 /// </summary>
 public class SelectBaseView : MonoBehaviour
 {
-    //[SerializeField] private float offsetRotateX = 0f; // ピッチのオフセット（視覚補正用）
-    //[SerializeField] private float offsetRotateY = 0f; // ヨーのオフセット（視覚補正用）
     [SerializeField] private float offsetRotateZ = 0f; // ロールのオフセット（視覚補正用）
-
-    //private float currentX = 0f;
-    //private float currentY = 0f;
     private float currentZ = 0f;
-
-    //public void RotateX(float rotateX)
-    //{
-    //    currentX = rotateX + offsetRotateX;
-    //    ApplyRotation();
-    //    //Debug.Log($"RotateX: {currentX} (offset: {offsetRotateX})");
-    //}
-
-    //public void RotateY(float rotateY)
-    //{
-    //    currentY = rotateY + offsetRotateY;
-    //    ApplyRotation();
-    //    //Debug.Log($"RotateY: {currentY} (offset: {offsetRotateY})");
-    //}
 
     public void RotateZ(float rotateZ)
     {
