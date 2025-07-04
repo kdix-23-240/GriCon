9f3112a16afc8dcf50ee655f1948669174e6b8d6
kdix-23-240 <akihide.3kami@gmail.com>
2025-07-04 23:46:01 +0900

    クリア時のファンファーレ実装

--- Assets/Script/ResultScene/ResultSceneDelay.cs
+++ Assets/Script/ResultScene/ResultSceneDelay.cs
@@ -12,16 +12,16 @@ public class ResultSceneDelay : MonoBehaviour
 
     private System.Collections.IEnumerator DelayCoroutine()
     {
-        // 操作不可能状態にする
-        GameSystem.canRotate = false;
-        GameSystem.canGrip = false;
-        GameSystem.canMove = false;
+        //// 操作不可能状態にする
+        //GameSystem.canRotate = false;
+        //GameSystem.canGrip = false;
+        //GameSystem.canMove = false;
 
         yield return new WaitForSeconds(delayTime); // 指定した時間だけ待機
 
-        // 操作可能状態に戻す
-        GameSystem.canRotate = true;
-        GameSystem.canGrip = true;
-        GameSystem.canMove = true;
+        //// 操作可能状態に戻す
+        //GameSystem.canRotate = true;
+        //GameSystem.canGrip = true;
+        //GameSystem.canMove = true;
     }
 }
