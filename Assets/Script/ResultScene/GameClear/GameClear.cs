499a4347cb30d34ff7a0aca7c21fcda9bde30009
kdix-23-240 <akihide.3kami@gmail.com>
2025-07-04 23:11:49 +0900

    クリア画面のアニメーション実装

--- Assets/Script/ResultScene/GameClear/GameClear.cs
+++ Assets/Script/ResultScene/GameClear/GameClear.cs
@@ -2,11 +2,19 @@ using UnityEngine;
 
 public class GameClear : MonoBehaviour
 {
+    private WarningPresenter warningPresenter;
+
+    private void Awake()
+    {
+        warningPresenter = new WarningPresenter();
+    }
     void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.name == "PlayerCollision")
         {
             Debug.Log("Game Clear!"); // ゲームクリアのメッセージをログに表示
+            // 警告レベル1が解除された場合、レベルを0に戻す
+            warningPresenter.WarningModel.WarningLevel.Value = 5;
             UnityEngine.SceneManagement.SceneManager.LoadScene("GameClear");
         }
     }
